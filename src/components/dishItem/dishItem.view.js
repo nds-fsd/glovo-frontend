defd7a39421a5084e0ee4688ca58cab42e6a53a1
DESKTOP-05JLC58\milec <milecofnas@gmail.com>
2021-03-26 20:48:36 +0100

    pages

--- src/components/dishItem/dishItem.view.js
+++ src/components/dishItem/dishItem.view.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import styles from './dishItem.module.css';
 
-export const DishItem = () => {};
+export const DishItem = ({item}) => {
+    return (
+        <div className={styles._container}>{item}</div>
+    )
+};
