4969a3ac7b06d376f23b21ea75be304b42d36e15
FelixCat <felixyvmdor@gmail.com>
2021-03-31 11:11:45 +0200

    navbar done

--- src/components/navBar/navBar.view.js
+++ src/components/navBar/navBar.view.js
@@ -1,4 +1,8 @@
-import React from 'react';
 import styles from './navBar.module.css';
 
-export const NavBar = () => {};
+export const NavBar = ({ children }) => (
+  <div className={styles.container}>
+    <h1>I am the CategoryNavbar </h1>
+    {children}
+  </div>
+);
