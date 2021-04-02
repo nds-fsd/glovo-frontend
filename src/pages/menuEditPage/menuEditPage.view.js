486a4915f5de54602fa894b4946b708bfaa2f060
Patricio <patomunoz.mx@gmail.com>
2021-04-02 23:32:12 +0200

    Modified DISHEXAMPLES to a real http response and adapted the list

--- src/pages/menuEditPage/menuEditPage.view.js
+++ src/pages/menuEditPage/menuEditPage.view.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './menuEditPage.module.css';
-import DishList from '../../components/dishList'
+import DishList from '../../components/dishList';
 
 export const MenuEditPage = () => {
-    return (
-        <div>
-            <DishList />
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <DishList />
+    </div>
+  );
 };
