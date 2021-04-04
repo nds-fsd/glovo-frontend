53f4fb7bc260e47c11b8c82c2889f34879063d6a
DESKTOP-05JLC58\milec <milecofnas@gmail.com>
2021-04-04 19:26:54 +0200

    modifications

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import RestaurantCreationPage from './pages/restaurantCreationPage';
-<<<<<<< HEAD
 import RestaurantViewPage from './pages/restaurantViewPage';
+// import RestaurantListPage from './pages/restaurantListPage';
 import { RESTAURANT_CREATION_PAGE, RESTAURANT_VIEW_PAGE } from './router/router';
-=======
-import RestaurantListPage from './pages/restaurantListPage';
-import { RESTAURANT_CREATION_PAGE, RESTAURANT_LIST_PAGE } from './router/router';
->>>>>>> Development
 
 function App() {
   return (
@@ -16,13 +12,8 @@ function App() {
         <Route path={RESTAURANT_CREATION_PAGE}>
           <RestaurantCreationPage />
         </Route>
-<<<<<<< HEAD
         <Route path={RESTAURANT_VIEW_PAGE}>
           <RestaurantViewPage />
-=======
-        <Route path={RESTAURANT_LIST_PAGE}>
-          <RestaurantListPage />
->>>>>>> Development
         </Route>
       </Switch>
     </Router>
