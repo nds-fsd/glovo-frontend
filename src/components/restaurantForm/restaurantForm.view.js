9f96ea4f1117fa53fc89e04a3f359f97c6daba65
Patricio <patomunoz.mx@gmail.com>
2021-04-06 18:28:39 +0200

    Added page and form features

--- src/components/restaurantForm/restaurantForm.view.js
+++ src/components/restaurantForm/restaurantForm.view.js
@@ -1,19 +1,38 @@
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { shortFetch } from '../../assets/utils/fetch.utils';
+import { RESTAURANT_CATEGORY } from '../../router/router';
 import { InputText } from '../inputText/inputText.view';
 import styles from './restaurantForm.module.css';
 
-export const RestaurantForm = () => {
+export const RestaurantForm = ({ enableButtons }) => {
+  const [categoryList, setCategoryList] = useState([]);
   const [name, setName] = useState();
-  const [nameError, setNameError] = useState(false);
+  const [category, setCategory] = useState();
   const [description, setDescription] = useState();
+  const [zipcode, setZipcode] = useState();
+  const [street, setStreet] = useState();
+  const [number, setNumber] = useState();
+  const [zipcodeError, setZipcodeError] = useState(false);
+  const [nameError, setNameError] = useState(false);
 
+  useEffect(() => {
+    shortFetch({ url: `${RESTAURANT_CATEGORY}`, onSuccess: setCategoryList, method: 'GET' });
+  }, []);
   const validateAndFetch = () => {
     if (!name) {
       setNameError(true);
     } else {
       setNameError(false);
     }
+    if (!zipcode || isNaN(zipcode) || zipcode < 10000) {
+      setZipcodeError(true);
+    } else {
+      setZipcodeError(false);
+    }
+    enableButtons();
   };
 
   return (
@@ -30,23 +49,49 @@ export const RestaurantForm = () => {
         />
       </div>
       <div className={`${styles.subContainer} ${styles.category}`}>
+        <select>
+          <option value="" selected disabled hidden>
+            Select a Category
+          </option>
+          {categoryList.map((cat) => (
+            <option value={cat._id}>{cat.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className={`${styles.subContainer} ${styles.description}`}>
         <textarea
           id="resDesc"
           className={styles.textarea}
+          value={description}
           onChange={(e) => setDescription(e.target.value)}
           placeholder="Please add your description"
         ></textarea>
       </div>
-      <div className={`${styles.subContainer} ${styles.description}`}>
-        <input type="text" placeholder="description" />
-      </div>
       <div className={`${styles.subContainer} ${styles.address}`}>
-        <input type="text" placeholder="street" />
-        <input type="text" placeholder="number" />
-        <input type="text" placeholder="Zip Code" />
+        <input
+          type="text"
+          value={street}
+          placeholder="street"
+          onChange={(e) => setStreet(e.target.value)}
+        />
+        <input
+          type="text"
+          value={number}
+          placeholder="number"
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <InputText
+          placeholder="Zipcode"
+          label="zipcode"
+          value={zipcode}
+          handleChange={setZipcode}
+          inputId="resZipcode"
+          error={zipcodeError}
+          errorMessage="Please add a valid zipcode"
+        />
       </div>
       <div className={`${styles.subContainer} ${styles.buttons}`}>
-        <button>cancel</button>
+        <button onClick={() => location.reload()}>cancel</button>
         <button onClick={validateAndFetch}>create</button>
       </div>
     </div>
