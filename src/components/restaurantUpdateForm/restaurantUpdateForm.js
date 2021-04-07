4b4af48f7b5f66632561bf325016243020b3d46c
DESKTOP-05JLC58\milec <milecofnas@gmail.com>
2021-04-07 21:03:17 +0200

    form

--- src/components/restaurantUpdateForm/restaurantUpdateForm.js
+++ src/components/restaurantUpdateForm/restaurantUpdateForm.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 import styles from './restaurantUpdateForm.module.css';
 import InputText from '../inputText';
+import CategorySelect from '../categorySelect';
 
 export const RestaurantUpdateForm = () => {
   const [updateName, setUpdateName] = useState();
+  // eslint-disable-next-line no-unused-vars
   const [updateCategory, setUpdateCategory] = useState();
   const [updateDescription, setUpdateDescription] = useState();
 
@@ -17,14 +20,7 @@ export const RestaurantUpdateForm = () => {
         inputId="resNewName"
         errorMessage="Please add a valid name"
       />
-      <InputText
-        placeholder="Enter a new category"
-        label="Category"
-        value={updateCategory}
-        handleChange={setUpdateCategory}
-        inputId="resNewCategory"
-        errorMessage="Please add a valid name"
-      />
+      <CategorySelect categoryValue={(value) => setUpdateCategory(value)} />
       <InputText
         placeholder="Enter a new description"
         label="Description"
