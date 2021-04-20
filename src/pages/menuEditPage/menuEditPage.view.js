cd8156b11ed2d53738f8c646e247671bd882638e
DESKTOP-05JLC58\milec <milecofnas@gmail.com>
2021-04-20 19:05:14 +0200

    log in and fixed menu edit page

--- src/pages/menuEditPage/menuEditPage.view.js
+++ src/pages/menuEditPage/menuEditPage.view.js
@@ -7,18 +7,21 @@ import DishModal from '../../components/dishModal';
 export const MenuEditPage = () => {
   const [handleModal, setHandleModal] = useState(false);
   const [selectedDish, setSelectedDish] = useState();
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className={styles.container}>
       <DishList
         onDishClick={(value) => setSelectedDish(value)}
         openModal={() => setHandleModal(true)}
+        toggle={toggle}
       />
 
       <DishModal
         open={handleModal}
         onClose={() => setHandleModal(false)}
         selectedDish={selectedDish}
+        onToggle={() => setToggle(!toggle)}
       />
     </div>
   );
