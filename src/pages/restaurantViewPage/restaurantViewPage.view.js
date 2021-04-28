93d98cc46057023c2f68114e15b3b4356cbbc023
DESKTOP-05JLC58\milec <milecofnas@gmail.com>
2021-04-28 20:59:05 +0200

    restaurant view page

--- src/pages/restaurantViewPage/restaurantViewPage.view.js
+++ src/pages/restaurantViewPage/restaurantViewPage.view.js
@@ -9,6 +9,7 @@ import Modal from '../../components/modal';
 import DishItem from '../../components/dishItem';
 import RestaurantUpdateForm from '../../components/forms/restaurantUpdateForm';
 import { ALL_COURSES, BACKEND, RESTAURANT } from '../../router/router';
+import headerImg from '../../assets/images/header-test.png';
 
 export const RestaurantViewPage = () => {
 	const [isOpenModal, setIsOpenModal] = useState(false);
@@ -35,6 +36,7 @@ export const RestaurantViewPage = () => {
 			})
 			.then((dishes) => {
 				setDishByCourse(dishes);
+				console.log(dishByCourse);
 			})
 			.catch((err) => {
 				return console.log(err);
@@ -43,45 +45,72 @@ export const RestaurantViewPage = () => {
 
 	return (
 		<div>
-			{selectedResto && (
-				<div className={styles._restoInfo}>
-					<p>{selectedResto.name}</p>
-					<p>{selectedResto.restaurantCategory.name}</p>
-					<p>{selectedResto.restaurantDescription}</p>
-					<button onClick={() => setIsOpenModal(true)}>Edit</button>
-				</div>
-			)}
-			{isOpenModal && (
-				<Modal
-					onClose={() => setIsOpenModal(false)}
-					open={isOpenModal}
-					title="Modify your data"
-				>
-					<RestaurantUpdateForm onClose={() => setIsOpenModal(false)} />
-				</Modal>
-			)}
-			<div className={styles._restoCourse}>
-				{selectedResto &&
-					selectedResto.courseList.map((course) => {
-						return (
-							<div>
-								<div className={styles._courses}>
-									<p onClick={() => handleClick(course._id)}>{course.name}</p>
-								</div>
-							</div>
-						);
-					})}
-				<Link to={`/menuEditPage/${id}`}>
-					<button>Edit Menu</button>
-				</Link>
-			</div>
-			<div>
-				{oneDish &&
-					oneDish[0].dishList.map((dish) => (
-						<div>
-							<DishItem selectedDish={dish} />
+			<header className={styles._headerContainer}>
+				<img src={headerImg} alt="food header" className={styles._headerImg} />
+			</header>
+			<div className={styles._allContainter}>
+				<div className={styles._restoCard}>
+					{selectedResto && (
+						<div className={styles._restoInfo}>
+							<p>
+								Category {'>'} {selectedResto.restaurantCategory.name}
+							</p>
+							<h1>{selectedResto.name}</h1>
+							<p>{selectedResto.restaurantDescription}</p>
+							<button onClick={() => setIsOpenModal(true)}>Edit</button>
 						</div>
-					))}
+					)}
+					{isOpenModal && (
+						<Modal
+							onClose={() => setIsOpenModal(false)}
+							open={isOpenModal}
+							title="Modify your data"
+						>
+							<RestaurantUpdateForm onClose={() => setIsOpenModal(false)} />
+						</Modal>
+					)}
+					<div className={styles._courseContainer}>
+						{selectedResto &&
+							selectedResto.courseList.map((course) => {
+								return (
+									<div>
+										<div className={styles._courses}>
+											<p onClick={() => handleClick(course._id)}>
+												{course.name}
+											</p>
+										</div>
+									</div>
+								);
+							})}
+						<Link to={`/menuEditPage/${id}`}>
+							<button>Edit Menu</button>
+						</Link>
+					</div>
+					<div className={styles._allDishes}>
+						{dishByCourse &&
+							dishByCourse.map((course) => {
+								return course.dishList.map((dish) => {
+									return (
+										<div>
+											<DishItem selectedDish={dish} />
+										</div>
+									);
+								});
+							})}
+					</div>
+					<div className={styles._dishesByClick}>
+						{oneDish &&
+							oneDish[0].dishList.map((dish) => (
+								<div>
+									<DishItem selectedDish={dish} />
+								</div>
+							))}
+					</div>
+				</div>
+
+				<div className={styles._infoGlovo}>
+					<p>Glovo Time</p>
+				</div>
 			</div>
 		</div>
 	);
