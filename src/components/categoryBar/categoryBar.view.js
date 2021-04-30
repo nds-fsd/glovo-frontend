ebfa08ca9d3e75d3b7a5ebf5680a798b380eceec
FelixCat <felixyvmdor@gmail.com>
2021-04-30 11:19:23 +0200

    right now it all works minor changes html and name

--- src/components/categoryBar/categoryBar.view.js
+++ src/components/categoryBar/categoryBar.view.js
@@ -1,19 +1,25 @@
 /* eslint-disable no-console */
 /* eslint-disable react/no-array-index-key */
 import classnames from 'classnames';
+// import { useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { RestoListContext } from '../context/restoListPageContext';
 import styles from './categoryBar.module.css';
 
 export const CategoryBar = ({ children }) => {
+  // function useQuery() {
+  //   return new URLSearchParams(useLocation().search);
+  // }
+  // let query = useQuery();
+  // let name = {query.get("name")}
   const { setCategorySelected, categorySelected } = useContext(RestoListContext);
   return (
     <div className={styles.container}>
       <ul className={styles.listContainer}>
         {children.map((item) => {
           return (
-            <Link to={`/category?name=${item.name}`}>
+            <Link to={`/restaurantListPage/category?name=${item.name}`}>
               <li
                 key={item._id}
                 className={classnames(styles.restaurantItem, {
